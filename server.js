dc0d0d978433f8bb51b9d0c6ae3614bc7f3f2d16
szmecskas <lidia.szm@gmail.com>
2021-12-20 08:27:11 +0200

    Connecting to server

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require("express")
+const http = require("http")
+const app = express()
+const server = http.createServer(app)
+const socket = require("socket.io")
+const io = socket(server)
+
+io.on("connection", (socket) => {
+    socket.emit("me", socket.id)
+
+    socket.on("disconnect", () => {
+        socket.broadcast.emit("callEnded")
+    })
+
+    
+})
+
+server.listen(5000, () => console.log("server is running on port 5000"))
